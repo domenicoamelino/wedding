f8bd1f6b7c499a51675858b2b67e0ca62addb52f
domenicoamelino <domenico.amelino@gmail.com>
2023-11-02 23:10:07 +0100

    go
    go

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbwgh_eNnmtv7Zjklsu105-lM-d1DEyfmfQTczQePoxhOz91tdy6rC_-LQCsC96-bEyx/exec', data)
+            $.post('hthttps://script.google.com/macros/s/AKfycbxyQ8Yj5danW8quL_fAVWlVqUj0TfR6zAbjKCsLZ4C4DNepzT1NX8bXDsQhNC59MpcK/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
